2f2f024c0ce7f887f52f54c8af7ae22abc640c80
AlexPraits0108 <Alejandro.medina@praits.com.mx>
2024-05-28 18:02:32 -0600

    shell routes section 16

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,24 +1,62 @@
 import 'package:cinemapedia/presentation/screens.dart';
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
+import 'package:cinemapedia/presentation/views/home_views/home_view.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: NameRoutes.homeScreen,
-      builder: (context, state) => const HomeScreen(),
-      routes: [
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  StatefulShellRoute.indexedStack(
+    builder: (context, state, child) {
+      return HomeScreen(childView: child);
+    },
+    branches: [
+      StatefulShellBranch(routes: [
         GoRoute(
-        path: 'movie/:id',
-        name: NameRoutes.movieScreen,
-        builder: (context, state) => MovieScreen( movieId: state.pathParameters['id'] ?? 'no-id'),
-      )
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: NameRoutes.movieScreen,
+              builder: (context, state) => MovieScreen( movieId: state.pathParameters['id'] ?? 'no-id'),
+            )
+          ]
+        ),
       ]
-    ),
-    
-  ]
-);
+      ),
+      StatefulShellBranch(
+        routes: [
+          GoRoute(
+            path: '/favorites',
+            builder: (context, state) {
+              return const FavoritesView();
+            },
+          ),
+        ]
+      )
+    ])
+]);
+
+//Rutas padre/Hijo
+// final appRouter = GoRouter(
+//   initialLocation: '/',
+//   routes: [
+//     GoRoute(
+//       path: '/',
+//       name: NameRoutes.homeScreen,
+//       builder: (context, state) => const HomeScreen( childView: HomeView(),),
+//       routes: [
+//         GoRoute(
+//         path: 'movie/:id',
+//         name: NameRoutes.movieScreen,
+//         builder: (context, state) => MovieScreen( movieId: state.pathParameters['id'] ?? 'no-id'),
+//       )
+//       ]
+//     ),
+
+//   ]
+// );
 
 class NameRoutes {
   static String homeScreen = 'homeScreen';
